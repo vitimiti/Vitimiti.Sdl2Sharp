54f9b44c9e9427fd634392211f68089ac6ef9263
Victor Matia <vmatir@gmail.com>
2023-01-07 03:45:46 +0000

    Add the first example.
    Added the first example for the library.

---
+++ Vitimiti.Sdl2Sharp.Examples/Vitimiti.Sdl2Sharp.Examples.HelloSdl/src/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Vitimiti.Sdl2Sharp.Examples.HelloSdl;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        using var app = new Systems(SubSystemsFlags.Video);
+        Console.WriteLine($"Running with subsystems [{Systems.InitializedSystemFlags}]");
+    }
+}
