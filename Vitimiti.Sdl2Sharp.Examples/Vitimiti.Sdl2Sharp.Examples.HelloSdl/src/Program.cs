a5beecdf9e683131804bdf090a09c0fc0c7e6330
Victor Matia <vmatir@gmail.com>
2023-01-07 04:06:11 +0000

    Added exception management.
    Added the exception that the library may launch.

--- Vitimiti.Sdl2Sharp.Examples/Vitimiti.Sdl2Sharp.Examples.HelloSdl/src/Program.cs
+++ Vitimiti.Sdl2Sharp.Examples/Vitimiti.Sdl2Sharp.Examples.HelloSdl/src/Program.cs
@@ -1,4 +1,6 @@
-﻿using Vitimiti.Sdl2Sharp.Utilities;
+﻿using System.Runtime.InteropServices;
+
+using Vitimiti.Sdl2Sharp.Utilities;
 
 namespace Vitimiti.Sdl2Sharp.Examples.HelloSdl;
 
@@ -6,12 +8,19 @@ internal static class Program
 {
     public static void Main()
     {
-        using var app = new Systems(SubSystemsFlags.Video);
-        Console.WriteLine($"Running SDL v{Systems.Version} on {Platform.Name} with subsystems [{Systems.InitializedSystemFlags}]");
-        if (Systems.Version > Systems.SupportedVersion || Systems.Version < Systems.SupportedVersion)
+        try
         {
-            var comparator = Systems.Version > Systems.SupportedVersion ? "higher" : "lower";
-            Console.WriteLine($"SDL v{Systems.Version} is {comparator} than the supported SDL v{Systems.SupportedVersion} and some functionality may not be available");
+            using var app = new Systems(SubSystemsFlags.Video);
+            Console.WriteLine($"Running SDL v{Systems.Version} on {Platform.Name} with subsystems [{Systems.InitializedSystemFlags}]");
+            if (Systems.Version > Systems.SupportedVersion || Systems.Version < Systems.SupportedVersion)
+            {
+                var comparator = Systems.Version > Systems.SupportedVersion ? "higher" : "lower";
+                Console.WriteLine($"SDL v{Systems.Version} is {comparator} than the supported SDL v{Systems.SupportedVersion} and some functionality may not be available");
+            }
+        }
+        catch (ExternalException e)
+        {
+            Console.Error.WriteLine(e);
         }
     }
 }
